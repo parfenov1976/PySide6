46cbbfb7ca4f000db34bd81118addf575b049444
parfenov1976 <parfenov1976@mail.ru>
2023-01-04 18:21:17 +0300

    modify 28_toolbars_and_menus_QToolBar.py

--- 1_Basic/28_toolbars_and_menus_QToolBar.py
+++ 1_Basic/28_toolbars_and_menus_QToolBar.py
@@ -1,5 +1,5 @@
 """
-Пример использования виджета QToolBar для создания панели меню с иконками.
+Пример использования виджета QToolBar для создания панели инструментов с иконками.
 """
 import sys
 import os
@@ -8,7 +8,8 @@ from PySide6.QtWidgets import (QApplication,
                                QMainWindow,
                                QLabel,
                                QToolBar,
-                               QStatusBar
+                               QStatusBar,
+                               QCheckBox
                                )
 from PySide6.QtGui import QAction, QIcon
 
@@ -21,7 +22,7 @@ from PySide6.QtGui import QAction, QIcon
 Qt - содержит различные идентификаторы, используемые в библиотеке Qt
 Импорт из модуля QtWidgets PySide6 класса для управления приложением QApplication и
 класса основного окна QMainWindow, класса виджета ярлыков (надписей) QLabel, класса виджета
-панели меню QToolBar.
+панели инструментов QToolBar.
 Импорт из модуля QtGui класса эффектов действия QAction, класса для создания иконок QIcon
 Другие виджеты можно найти по ссылке https://doc.qt.io/qt-5/widget-classes.html#basic-widget-classes
 """
@@ -43,31 +44,44 @@ class MainWindow(QMainWindow):
         self.label = QLabel('Hello!')  # создание экземпляра класса виджета ярлыка с надписью
         self.label.setAlignment(Qt.AlignCenter)  # установка настройки с центральным расположением надписи
         self.setCentralWidget(self.label)  # размещение надписи в главном окне приложения
-        self.toolbar = QToolBar('My main toolbar')  # создание экземпляра панели меню с указанием имени панели
+        self.toolbar = QToolBar('My main toolbar')  # создание экземпляра панели инструментов с указанием имени панели
         self.toolbar.setIconSize(QSize(16, 16))  # установка размера иконки
-        self.addToolBar(self.toolbar)  # размещение панели меню в главном окне приложения
+        self.addToolBar(self.toolbar)  # размещение панели инструментов в главном окне приложения
         self.button_action = QAction(QIcon(os.path.join(basedir, 'data', 'bug.png')), 'Your button', self)
         """
-        Создание эффекта для кнопки панели меню. Данный эффект можно подключать к разным элементам интерфейса,
+        Создание эффекта для кнопки панели инструментов. Данный эффект можно подключать к разным элементам интерфейса,
         которые выполняют одну и туже функцию для избежания многократного дублирования определения действий. 
-        Текстовая строка задает имя кнопки. QIcon устанавливает иконку для кнопки.
+        Текстовая строка задает имя кнопки. QIcon устанавливает иконку для кнопки. Текст по умолчанию будет
+        отображаться в облачке с подсказкой при удержании курсора над иконкой.
         """
-        self.button_action.setStatusTip('This is your button')  # установка строки, выводимой в панели статуса
-        # наведении курсора на кнопку
-        self.button_action.triggered.connect(self.on_my_tool_bar_button_click)  # создание согнала с привязкой
+        self.button_action.setStatusTip('This is your button')  # установка строки подсказки, выводимой в панели статуса
+        # при наведении курсора на кнопку
+        self.button_action.triggered.connect(self.on_my_tool_bar_button_click)  # создание сигнала с привязкой
         # метода ресивера
         self.button_action.setCheckable(True)  # Устанавливает кнопку в режим переключателя. Если данный режим
         # не установлен, то при нажатии на кнопку сигнал всегда будет передавать False.
-        self.toolbar.addAction(self.button_action)  # привязка эффекта к панели меню
+        self.toolbar.addAction(self.button_action)  # привязка эффекта к панели инструментов
+        self.toolbar.addSeparator()  # добавление разделителя между кнопками
+        # далее повторяем код для создания второй кнопки
+        self.button_action2 = QAction(QIcon(os.path.join(basedir, 'data', 'bug.png')), 'Your second button', self)
+        self.button_action2.setStatusTip('This is your second button')
+        self.button_action2.triggered.connect(self.on_my_tool_bar_button_click)
+        self.button_action2.setCheckable(True)
+        self.toolbar.addAction(self.button_action2)
+        self.toolbar.addWidget(QLabel('Hello world!!!'))  # добавляет виджет ярлыка с надписью на панель инструментов
+        # self.toolbar.addWidget(QCheckBox())  # добавляет чек бокс на панель инструментов
+        self.checkbox_widget = QCheckBox('Your checkbox')
+        self.checkbox_widget.setStatusTip('This is your checkbox')
+        self.toolbar.addWidget(self.checkbox_widget)
         self.setStatusBar(QStatusBar(self))  # Создание экземпляра виджета панели статуса и
-        # его размещение в главном окне приложения. При наведении курсора на кнопку панели меню
+        # его размещение в главном окне приложения. При наведении курсора на кнопку панели инструментов
         # в статусе будет отображаться строка статуса кнопки
 
     @staticmethod
     def on_my_tool_bar_button_click(s: bool) -> None:
         """
         Метод ресивер (слот) для получения сигнала и его вывода.
-        :param s: bool - содержимое сигнала от виджета.
+        :param s: bool - содержимое сигнала от виджета
         :return: None
         """
         print('click', s)
