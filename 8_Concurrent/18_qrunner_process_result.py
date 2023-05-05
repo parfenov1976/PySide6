ff71e3413bf9ae62ed317286cd1ccea9ad4e864e
parfenov1976 <parfenov1976@mail.ru>
2023-05-05 21:44:37 +0300

    modify document lines in 18_qrunner_process_result.py

--- 8_Concurrent/18_qrunner_process_result.py
+++ 8_Concurrent/18_qrunner_process_result.py
@@ -145,7 +145,7 @@ class MainWindow(QMainWindow):
         # создание экземпляра рабочего потока для запуска кода внешнего процесса dummy_script.py с передачей ссылки
         # на функцию обработчик результатов работы внешнего процесса extract_vars
         self.runner.signals.result.connect(self.result)  # создание сигнала на передачу результата с привязкой ресивера
-        self.threadpool.start(self.runner)  # создание сигнала на завершение выполнения внешнего процесса
+        self.threadpool.start(self.runner)  # запуск рабочего потока
 
     def result(self, data: dict) -> None:
         """
