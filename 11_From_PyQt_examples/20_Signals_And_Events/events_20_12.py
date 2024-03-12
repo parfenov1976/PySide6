cba016d6abbb248adf6c34c31bc430aca8ff22f4
parfenov1976 <parfenov1976@mail.ru>
2024-03-12 23:10:09 +0300

    Modify events_20_12.py

--- 11_From_PyQt_examples/20_Signals_And_Events/events_20_12.py
+++ 11_From_PyQt_examples/20_Signals_And_Events/events_20_12.py
@@ -1,5 +1,6 @@
 """
 Обработка событий клавиатуры. Управление фокусом ввода.
+Переключение фокуса ввода производится нажатием клавиши <Tab>
 
 Для управления фокусом ввода предназначены следующие методы класса QWidget:
 ♦ setFocus ([<Причина>]) - устанавливает фокус ввода, если компонент находится в активном
@@ -66,20 +67,6 @@ QWidget.
   если был получен;
 ♦ reason() - возвращает причину установки фокуса. Значение аналогично значению
   параметра <Причина> в методе setFocus().
-Для назначения клавиш быстрого доступа также можно воспользоваться классом QShortcut
-из модуля QtGui. В этом случае назначение клавиши у первого текстового поля будет выглядеть
-так:
-self.line1 = QtWidgets.QLineEdit()
-self.shc = QtGui.QShortcut(QtGui.QKeySequence.mnemonic("&e"), self)
-self.shc.setContext(QtCore.Qt.ShortcutContext.WindowShortcut)
-self.shc.activated.connect(self.line1.setFocus)
-Еще можно использовать класс QAction из модуля QtGui. Назначение клавиши у второго
-текстового поля выглядит следующим образом:
-self.line2 = QtWidgets.QLineEdit()
-self.act = QtGui.QAction(self)
-self.act.setShortcut(QtGui.QKeySequence.mnemonic("&r"))
-self.act.triggered.connect(self.line2.setFocus)
-self.addAction(self.act)
 """
 import sys
 from PySide6.QtWidgets import (QApplication,
@@ -89,11 +76,6 @@ from PySide6.QtWidgets import (QApplication,
                                QVBoxLayout,
                                QWidget,
                                )
-from PySide6.QtGui import (QShortcut,
-                           QKeySequence,
-                           QAction,
-                           )
-from PySide6.QtCore import Qt
 """
 Импорт модуля sys, предоставляющего доступ к объекта интерпретатора, нужен для доступа
 к аргументам командной строки. Если использование аргументов командной строки не предполагается,
@@ -103,11 +85,6 @@ from PySide6.QtCore import Qt
 Импорт из модуля PySide6.QWidgets класса управления приложением QApplication, класса главных окон QMainWindow,
 класс виджета кнопки QPushButton, класс слоя с вертикальной организацией виджетов QVBoxLayout,
 базовый класс пустого виджета QWidget, класс редактируемого однострочного текстового поля QLindeEdit
-
-Импорт из модула PySide6.QtGui класса горячих клавиш QShortcut, класса сочетаний клавиш QKeySequence,
-класса действий QAction
-
-Импорт из модуля PySide6.QtCore класса перечислителя настроек виджетов Qt
 """
 
 
@@ -170,15 +147,6 @@ class MainWindow(QMainWindow):
         self.btn.clicked.connect(self.on_clicked)  # назначение обработчика сигналу на нажатие кнопки
         QMainWindow.setTabOrder(self.line1, self.line2)  # задаем порядок обхода с помощью клавиши <Tab>
         QMainWindow.setTabOrder(self.line2, self.btn)  # задаем порядок обхода с помощью клавиши <Tab>
-
-        self.shc = QShortcut(QKeySequence.mnemonic("&e"), self)  # создание сочетания горячих клавиш <Alt>+<e>
-        self.shc.setContext(Qt.ShortcutContext.WindowShortcut)  # добавление сочетания к контексту окна приложения
-        self.shc.activated.connect(self.line1.setFocus)  # создание сигнала на активацию фокуса и привязка обработчика
-
-        self.act = QAction(self)  # создание экземпляра класса действия
-        self.act.setShortcut(QKeySequence.mnemonic('&r'))  # создание сочетания горячих клавиш <Alt>+<r>
-        self.act.triggered.connect(self.line2.setFocus)  # создание сигнала на активацию фокуса и привязка обработчика
-        self.addAction(self.act)  # добавление действия в экземпляр окна приложения
 
     def on_clicked(self) -> None:
         """
