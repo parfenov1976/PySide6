cf1ab4ca08679923dc5ebaf7e7cb8f0a4024b7bc
parfenov1976 <parfenov1976@mail.ru>
2024-07-05 15:35:27 +0300

    Modify standarditem_23_4_4.py

--- 11_From_PyQt_examples/23_Lists_and_tables/23_4_Models/standarditem_23_4_4.py
+++ 11_From_PyQt_examples/23_Lists_and_tables/23_4_Models/standarditem_23_4_4.py
@@ -116,4 +116,75 @@ QStandardItem(<Количество строк>[, <Количество стол
   перечисления sortOrder из модуля QtCore.Qt, сортировка производится в прямом порядке,
   а если элемент DescendingOrder того же перечисления - в обратном.
 """
-# TODO добавить пример
+from PySide6.QtWidgets import (QMainWindow,
+                               QTreeView,
+                               )
+from PySide6.QtGui import QStandardItem, QStandardItemModel
+"""
+Импорт из модуля PySide6.QtWidgets класса главных окон QMainWindow,
+класса представления иерархического списка QTreeView
+
+Импорт из модуля PySide6.QtGui класса элемента модели QStandardItem,
+класса стандартной двухмерная модели QStandardItemModel
+"""
+
+
+class MainWindow(QMainWindow):
+    """
+    Класс главного окна приложения от супер класса главных окон
+    """
+
+    def __init__(self, parent=None) -> None:
+        """
+        Конструктор главного окна приложения
+        :param parent: ссылка на родительский объект верхнего уровня
+        """
+        # super(parent).__init__()  # вызов конструктора родительского класса через функцию super()
+        QMainWindow.__init__(self, parent)  # явный вызов конструктора родительского класса
+        self.setWindowTitle('Класс элемента модели')  # установка заголовка главного окна приложения
+        self.resize(300, 300)  # установка исходного размера главного окна приложения
+        self.tree_view = QTreeView()  # создание представления иерархического списка
+        self.standard_item_model = QStandardItemModel()  # создание модели иерархического списка
+        # сборка иерархического списка
+        rootitem_1 = QStandardItem('QAbstractItemView')
+        rootitem_2 = QStandardItem('Базовый класс')
+        item_1 = QStandardItem('QListView')
+        item_1.setToolTip('Список')  # добавление подсказки к элементу
+        item_2 = QStandardItem('Список')
+        item_2.setCheckable(True)
+        rootitem_1.appendRow([item_1, item_2])
+        item_3 = QStandardItem('QTableView')
+        item_4 = QStandardItem('Таблица')
+        rootitem_1.appendRow([item_3, item_4])
+        item_5 = QStandardItem('QTreeView')
+        item_6 = QStandardItem('Иерархический список')
+        rootitem_1.appendRow([item_5, item_6])
+        self.standard_item_model.appendRow([rootitem_1, rootitem_2])
+        self.standard_item_model.setHorizontalHeaderLabels(['Класс', 'Описание'])
+        self.standard_item_model.itemChanged.connect(lambda item: print(f'{item.text()} - {item.checkState()}'))
+        # обработчик сигнала на изменение элемента списка
+
+        self.tree_view.setModel(self.standard_item_model)  # подключение модели к представлению
+        self.tree_view.setColumnWidth(0, 170)
+        self.setCentralWidget(self.tree_view)  # размещение представления в главном окне приложения
+
+
+if __name__ == '__main__':  # проверка условия запуска для предотвращения исполнения
+    # кода верхнего уровня при импортировании данного файла как модуля
+    from PySide6.QtWidgets import QApplication
+    import sys
+
+    """
+    Импорт из модуля PySide6.QtWidgets класса управления приложением QApplication
+    Импорт модуля sys, предоставляющего доступ к объекта интерпретатора, нужен для доступа
+    к аргументам командной строки. Если использование аргументов командной строки не предполагается,
+    то импорт можно не выполнять. При этом, при создании приложения в класс QtWidgets.QApplication([])
+    в качестве аргумента передается пустой.
+    """
+    app = QApplication(sys.argv)  # создание основного цикла событий приложения
+    app.setStyle('Fusion')  # установка более красивого стиля оформления графического интерфейса
+    window = MainWindow()  # создание главного окна приложения
+    window.show()  # включение видимости окна, по умолчанию окно спрятано
+    sys.exit(app.exec())  # Запуск основного цикла событий приложения.
+    # Код ниже метода запуска цикла событий не будет достигнут и выполнен пока не будет выполнен
+    # выход и цикл событий не будет остановлен. Не обязательно оборачивать запуск цикла в метод sys.exit()
